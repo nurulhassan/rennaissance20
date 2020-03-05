bfe8ff0434207b46c0087bec7c88a4ab5da2b7df
Jayesh Joshi <python.jjlearner@gmail.com>
2020-03-05 19:42:38 +0530

    mobile background changes and navbar update

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -2,12 +2,5 @@ $(document).ready(function(){
 	$('#MyImg').click(function(){
   		$('#Mymodal').modal('show')
 	});
-  function hideLoader() {
-    $('#loading').fadeOut('slow',function(){});
-}
 
-$(window).ready(hideLoader);
-
-// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
-setTimeout(hideLoader, 1000);
 });
