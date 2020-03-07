3111a1affeabc06a1c424d4992e42bc91c389b2d
Jayesh Joshi <python.jjlearner@gmail.com>
2020-03-07 15:35:38 +0530

    zarurat page added quanta page added

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -3,6 +3,17 @@ $(document).ready(function(){
   		$('#Mymodal').modal('show')
 	});
 
-	
+	$('.certificate_new').on('scroll',()=>{
+	$('.nav-pills').css("position","fixed")
+	$('.nav-pills').css("background-color","white")
+  $('.tab-content').css("padding-top","90px;")
+	$('.nav-pills').css("width","50%")
+	if(DocWidth>=500){
+		$('.tab-content').css("margin-top","20px")
+	}
+	else{
+		$('.tab-content').css("margin-top","53px")
+	}
+})
 
 });
