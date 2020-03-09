cdd9c652956c3ad72c344c3384a557ea375dc0e2
Jayesh Joshi <python.jjlearner@gmail.com>
2020-03-09 23:41:42 +0530

    nav pages added and minor changes

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -3,25 +3,13 @@ $(document).ready(function(){
   		$('#Mymodal').modal('show')
 	});
 
-	var DocWidth = $(window).width()
-
-	$('.certificate_new').on('scroll',()=>{
-	$('.nav-pills').css("position","fixed")
-	$('.nav-pills').css("background-color","orange")
-  $('.tab-content').css("margin-top","30px");
-	$(window).on('resize',function(){
-    if ((DocWidth>=981) || (DocWidth<981)) {
-      location.reload();  // refresh page
-    }
+	$(".r-logo").click(function(e){
+	  $(e.currentTarget).removeClass("r-animate"); 
 	});
-	if(DocWidth>=981){
-		$('.nav-pills').css("width","50%")
-	}
-	else{
-		$('.nav-pills').css("width","80%")
-	}
 
-})
+	$("#animator").click(function(e){
+	  $('.r-logo').addClass("r-animate");
+	});
 
 
 
